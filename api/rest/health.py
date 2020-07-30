c3f1fb16bf8aa81289fc81f82209b9e746573c60
yjh0310 <54786164+yjh0310@users.noreply.github.com>
2020-07-30 15:52:38 +0900

    initial commit

---
+++ api/rest/health.py
@@ -0,0 +1,5 @@
+from libs.status import Status
+
+
+def hi():
+    return {'okay': True}, Status.HTTP_200_OK
---
+++ libs/database/types.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class Base:
+    '''
+    https://docs.sqlalchemy.org/en/latest/orm/extensions/declarative/mixins.html
+    '''
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    def json(self, **kwargs):
+        return {}
+
+Base = declarative_base(cls=Base)
+
+class TwolipTypes:
+    pass
