9f057849eefe2bd0b0d675ca0fa6c8086a5b6a45
yjh0310 <54786164+yjh0310@users.noreply.github.com>
2020-07-30 17:01:43 +0900

    initial commit

--- api/rest/health.py
+++ api/rest/health.py
@@ -1,5 +1,6 @@
+from libs.route.router import route
 from libs.status import Status
 
-
+@route
 def hi():
     return {'okay': True}, Status.HTTP_200_OK
---
+++ libs/sms.py
@@ -0,0 +1,19 @@
+import requests
+
+
+class SmsHelper:
+    HOST = 'https://api-sms.cloud.toast.com'
+
+    def __init__(self, APPKEY, sender_no):
+        self.APPKEY = APPKEY
+        self.sender_no = sender_no
+
+    def send_auth_sms(self, sms):
+        sms_result = requests.post(f"{self.HOST}/sms/v2.1/appKeys/{self.APPKEY}/sender/sms",
+                                   json={
+                'body': f'[클론코딩] 인증번호는 다음과 같습니다: {sms.auth_value}',
+                'sendNo': self.sender_no,
+                'recipientList': [{'recipientNo': sms.phone_num}]
+            }).json()
+
+        return sms_result
